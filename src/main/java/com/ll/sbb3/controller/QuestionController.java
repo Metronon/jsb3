54907048686712001fb96fd24d32596c120f3335
Metronon <bumpercar07@gmail.com>
2024-12-29 21:29:26 +0900

    2-07

--- src/main/java/com/ll/sbb3/controller/QuestionController.java
+++ src/main/java/com/ll/sbb3/controller/QuestionController.java
@@ -1,7 +1,26 @@
 package com.ll.sbb3.controller;
 
+import com.ll.sbb3.entity.Question;
+import com.ll.sbb3.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/question")
 public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
 }
