6bd77f4128767c54ec95646ead9d5f5245a9cdb5
Metronon <bumpercar07@gmail.com>
2024-12-29 21:56:50 +0900

    2-12

--- src/main/java/com/ll/sbb3/controller/AnswerController.java
+++ src/main/java/com/ll/sbb3/controller/AnswerController.java
@@ -1,7 +1,32 @@
 package com.ll.sbb3.controller;
 
+import com.ll.sbb3.entity.Question;
+import com.ll.sbb3.service.AnswerService;
+import com.ll.sbb3.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/answer")
 public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(
+            Model model,
+            @PathVariable("id") Integer id,
+            @RequestParam(value="content") String content
+    ) {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
 }
