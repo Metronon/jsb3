bd39e7cfeecfda6d65b2aaf2b876e13f76e26334
Metronon <bumpercar07@gmail.com>
2024-12-29 21:48:55 +0900

    2-10

--- src/main/java/com/ll/sbb3/controller/QuestionController.java
+++ src/main/java/com/ll/sbb3/controller/QuestionController.java
@@ -1,12 +1,12 @@
 package com.ll.sbb3.controller;
 
 import com.ll.sbb3.entity.Question;
-import com.ll.sbb3.repository.QuestionRepository;
 import com.ll.sbb3.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,12 +16,19 @@ import java.util.List;
 @RequestMapping("/question")
 public class QuestionController {
 
-    private final QuestionService questionService
+    private final QuestionService questionService;
 
     @GetMapping("/list")
     public String list(Model model){
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/ll/sbb3/service/QuestionService.java
+++ src/main/java/com/ll/sbb3/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.ll.sbb3.service;
 
 import com.ll.sbb3.entity.Question;
+import com.ll.sbb3.global.exception.DataNotFoundException;
 import com.ll.sbb3.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +17,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
