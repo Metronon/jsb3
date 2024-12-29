c0d87dbffbd5cea0b41ff267c5295cd358095955
Metronon <bumpercar07@gmail.com>
2024-12-29 20:41:34 +0900

    2-05

---
+++ src/main/java/com/ll/sbb3/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sbb3.repository;
+
+import com.ll.sbb3.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
--- src/test/java/com/ll/sbb3/Sbb3ApplicationTests.java
+++ src/test/java/com/ll/sbb3/Sbb3ApplicationTests.java
@@ -1,13 +1,40 @@
 package com.ll.sbb3;
 
+import com.ll.sbb3.entity.Answer;
+import com.ll.sbb3.entity.Question;
+import com.ll.sbb3.repository.AnswerRepository;
+import com.ll.sbb3.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
-class Sbb3ApplicationTests {
+class SbbApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
 
     @Test
-    void contextLoads() {
-    }
+    @Transactional
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    }
 }
