c6b8e11a9a6abf70e16bbc27c33cb345aebd78a4
Metronon <bumpercar07@gmail.com>
2025-01-11 17:14:41 +0900

    3-07

--- src/main/java/com/ll/sbb3/repository/UserRepository.java
+++ src/main/java/com/ll/sbb3/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.ll.sbb3.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
+
+    Optional<SiteUser> findByusername(String username);
 }
