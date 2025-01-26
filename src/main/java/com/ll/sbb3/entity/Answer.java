fb1cfa659951ffa6c1d03e0eb6c06d72d0236036
Metronon <bumpercar07@gmail.com>
2025-01-26 20:55:43 +0900

    3-09 modifyDate

--- src/main/java/com/ll/sbb3/entity/Answer.java
+++ src/main/java/com/ll/sbb3/entity/Answer.java
@@ -1,10 +1,15 @@
 package com.ll.sbb3.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,6 +23,8 @@ public class Answer {
     private String content;
 
     private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
 
     @ManyToOne
     private Question question;
--- src/main/java/com/ll/sbb3/entity/Question.java
+++ src/main/java/com/ll/sbb3/entity/Question.java
@@ -1,11 +1,18 @@
 package com.ll.sbb3.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -22,6 +29,8 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate; // 생성일시 LocalDateTime (밀리초까지 표시)
+
+    private LocalDateTime modifyDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) // CascadeType Remove 설정으로 Question 삭제시 AnswerList 전부 삭제
     private List<Answer> answerList;
